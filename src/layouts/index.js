b30ec6efc2c6fe7eac0d356e7ece1770055c337f
Stackbit Code Editor <projects@stackbit.com>
2021-03-16 02:47:56 +0000

    index.js: changed

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import advanced from './advanced';
 import page from './page';
 import post from './post';
+<link  rel = "stylesheet"  href = "https://latex.now.sh/style.min.css"  />
 
 export {
     advanced,
